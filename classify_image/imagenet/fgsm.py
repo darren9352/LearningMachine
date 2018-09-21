6e98a95c471871d23178c41c5387b20e422e39a6
hakhyunkim <hhkim94@naver.com>
2018-09-21 10:43:29 +0900

    implement noise, attack speed.

--- classify_image/imagenet/fgsm.py
+++ classify_image/imagenet/fgsm.py
@@ -59,17 +59,20 @@ def save_image(adv_img, noise, path):
     sv_img.save(path)
 
 def deepfool_attack(model, n, x_input, input_img, sess):
+    import time
+    start_time = time.time() 
     wrap = KerasModelWrapper(model)
     deepfool = DeepFool(wrap, sess=sess)
     adv_x = deepfool.generate(x=x_input, over_shoot=0.02, max_iter=n,
         nb_candidate=2, clip_min=-1., clip_max=1.)
     adv_img = sess.run(adv_x, feed_dict={x_input: input_img})
+    attack_time = time.time() - start_time
 
     noise = input_img[0] - adv_img[0]
     #save_image(noise)
     save_image(adv_img, noise, 'output/testtest.png')
     preds = model.predict(adv_img)
-    return decode_predictions(preds, top=10)[0]
+    return decode_predictions(preds, top=10)[0], attack_time
 
 def cw_attack_keras(model, x_input, input_img, sess, n):
     wrap = KerasModelWrapper(model)
@@ -94,7 +97,6 @@ def cw_attack_keras(model, x_input, input_img, sess, n):
 def fgsm_attack_iter(model, x_input, input_img, sess, n):
     wrap = KerasModelWrapper(model)
     eps = 2.0 * 16 / 255.0
-    #eps = 0.1
     fgsm = FastGradientMethod(wrap)
     x_adv = fgsm.generate(x_input, eps=eps, clip_min=-1., clip_max=1.)
     adv_image = sess.run(x_adv, feed_dict={x_input: input_img})
@@ -110,23 +112,31 @@ def fgsm_attack_iter(model, x_input, input_img, sess, n):
 def attack(algorithm, n, d, x_input, x, sess):
     print(algorithm, 'attack is start')
     if algorithm == 'FGSM':
-        result = fgsm_attack(d, n, x_input, x, sess)
+        result, attack_time = fgsm_attack(d, n, x_input, x, sess)
     elif algorithm == 'CWL2':
-        result = cw_attack(d, n, x_input, x, sess)
+        result, attack_time = cw_attack(d, n, x_input, x, sess)
     elif algorithm == 'DeepFool' :
-        result = deepfool_attack(d, n, x_input, x, sess)
-    return result
+        result, attack_time = deepfool_attack(d, n, x_input, x, sess)
+    return result, attack_time
 
 def fgsm_attack(d, n, x_input, x, sess) :
+    import time
+    start_time = time.time() 
     res = fgsm_attack_iter(d, x_input, x, sess, n)
+    attack_time = time.time() - start_time
+
     noise = res[0] - x[0]
     save_image(res, noise, 'output/testtest.png')
     preds = d.predict(res)
-    return decode_predictions(preds, top=10)[0]
+    return decode_predictions(preds, top=10)[0], attack_time
 
 def cw_attack(d, n, x_input, x, sess) :
+    import time
+    start_time = time.time() 
     res = cw_attack_keras(d, x_input, x, sess, n)
+    attack_time = time.time() - start_time
+
     noise = res[0] - x[0]
     save_image(res, noise, 'output/testtest.png')
     preds = d.predict(res)
-    return decode_predictions(preds, top=10)[0]
+    return decode_predictions(preds, top=10)[0], attack_time
